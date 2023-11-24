2f763308071be7a7854546140226f6291676d6f9
TheRealAsciiMan <endvad.galaxy@gmail.com>
2023-11-24 15:25:39 +0100

    En cours

--- cellules.py
+++ cellules.py
@@ -1,16 +1,65 @@
-
+from random import *
 
 class Grille:
     largeur = None
     hauteur = None
     matrix = [[]]
-    def __int__(self, larg, haut):
+    def __init__(self, larg, haut):
         self.largeur = larg
         self.hauteur = haut
 
-    def dans_grille(self):
+    def dans_grille(self, pixel):
+        if self.matrix[pixel[0][pixel[1]]] in self.matrix:
+            return True
+        else:
+            return False
+    def setXY(self, boole, pixel):
+        self.matrix[pixel[0][pixel[1]]] = boole
+    def getXY(self, pixel):
+        return self.matrix[pixel[0][pixel[1]]]
+
+    def get_largeur(self):
+        return self.largeur
+
+    def get_hauteur(self):
+        return self.hauteur
+
+    def est_voisin(self, pixel1, pixel2):
+        if abs(pixel1[0]-pixel2[0]) < 1 and abs(pixel1[1]-pixel2[1]):
+            return True
+        else:
+            return False
+
+    def get_voisins(self, pixel):
+        return self.matrix[pixel[0][pixel[1]]].get_voisins
+
+    def affecte_voisins(self):
         for i in self.matrix:
             for j in self.matrix:
+                self.matrix[i[j]].set_voisins(self.get_voisins((i, j)))
+
+    def __str__(self):
+        ret = ""
+        for i in self.matrix:
+            ret += "\n"
+            for j in self.matrix:
+                ret += self.matrix[i[j]].__str__()
+        return ret
+
+    def remplir_alea(self, taux):
+        for i in range(self.largeur):
+            for j in range(self.hauteur):
+                if randint
+                self.matrix[j[i]] ==  Cellule
+
+                self.matrix[j[i]].naitre
+
+
+        return True
+
+
+
+
 
 
 
