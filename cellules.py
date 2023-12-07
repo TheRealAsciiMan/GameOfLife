6e89bda598cf117ae2d20a7ef69403d25109628d
TheRealAsciiMan <147166455+TheRealAsciiMan@users.noreply.github.com>
2023-12-07 23:03:27 +0100

    Projet Final

--- cellules.py
+++ cellules.py
@@ -4,7 +4,39 @@ from random import randint
 #j hauteur
 
 class Grille:
-    def __init__(self, larg, haut):
+    """
+    Représente une grille pour le Jeu de la Vie.
+
+    Attributs:
+        largeur (int): La largeur de la grille.
+        hauteur (int): La hauteur de la grille.
+        matrix (list[list[Cellule]]): Une matrice de cellules représentant l'état actuel de la grille.
+
+    Méthodes:
+        __init__(self, larg: int, haut: int)
+        dans_grille(self, l: int, h: int) -> bool
+        setXY(self, l: int, h: int, new: bool)
+        getXY(self, l: int, h: int) -> Cellule
+        get_largeur(self) -> int
+        get_hauteur(self) -> int
+        est_voisin(l: int, h: int, x: int, y: int) -> bool
+        get_voisins(self, l: int, h: int) -> list[Cellule]
+        affecte_voisins(self)
+        __str__(self) -> str
+        remplir_alea(self, taux: int)
+        jeu(self)
+        actualise(self)
+    """
+    def __init__(self, larg:int, haut:int):
+        """
+        Initialise une grille de jeu.
+
+        Crée une grille de jeu avec une largeur et une hauteur données et remplit la grille de cellules mortes.
+
+        Paramètres:
+            larg (int): La largeur de la grille.
+            haut (int): La hauteur de la grille.
+        """
         self.largeur = larg
         self.hauteur = haut
         self.matrix = []
@@ -14,28 +46,106 @@ class Grille:
                 ligne.append(Cellule())
             self.matrix.append(ligne)
 
-    def dans_grille(self, l, h):
+    def dans_grille(self, l:int, h:int)->bool:
+        """
+        Vérifie si les coordonnées (l, h) sont à l'intérieur de la grille.
+
+        Vérifie si les coordonnées sont valides dans la grille grâce à la largeur et la hauteur.
+
+        Paramètres:
+            l (int): Coordonnée en largeur.
+            h (int): Coordonnée en hauteur.
+
+        Retour:
+            bool: True si les coordonnées sont à l'intérieur de la grille, sinon False.
+        """
         return 0 <= l <= self.largeur and 0 <= h <= self.hauteur
 
-    def setXY(self, l, h, new):
+    def setXY(self, l:int, h:int, new:bool):
+        """
+        Modifie l'état d'une cellule aux coordonnées (l, h).
+
+        Modifie l'état d'une cellule aux coordonnées spécifiées.
+
+        Paramètres:
+            l (int): Coordonnée en largeur.
+            h (int): Coordonnée en hauteur.
+            new (bool): Nouvel état de la cellule.
+        """
         self.matrix[h][l].actuel = new
 
-    def getXY(self, l, h):
+    def getXY(self, l:int, h:int):
+        """
+        Récupère la cellule aux coordonnées (l, h).
+
+        Récupère la cellule spécifiée par ses coordonnées dans la matrix.
+
+        Paramètres:
+            l (int): Coordonnée en largeur.
+            h (int): Coordonnée en hauteur.
+
+        Retour:
+            Cellule: La cellule aux coordonnées spécifiées.
+        """
         return self.matrix[h][l]
 
-    def get_largeur(self):
+    def get_largeur(self)->int:
+        """
+        Récupère la largeur de la grille.
+
+        Récupère la largeur actuelle de la grille avec l'attribut largeur.
+
+        Retour:
+            int: La largeur de la grille.
+        """
         return self.largeur
 
-    def get_hauteur(self):
+    def get_hauteur(self)->int:
+        """
+        Récupère la hauteur de la grille.
+
+        Récupère la hauteur actuelle de la grille avec l'attribut hauteur.
+
+        Retour:
+            int: La hauteur de la grille.
+        """
         return self.hauteur
 
     @staticmethod
-    def est_voisin(l, h, x, y):
+    def est_voisin(l:int, h:int, x:int, y:int)->bool:
+        """
+        Méthode Statique
+
+        Vérifie si les coordonnées (x, y) sont des voisins de la cellule aux coordonnées (l, h).
+
+        Vérifie si les coordonnées spécifiées sont des voisins de la cellule en calculant la distance qui les sépare.
+
+        Paramètres:
+            l (int): Coordonnée en largeur.
+            h (int): Coordonnée en hauteur.
+            x (int): Coordonnée x du voisin potentiel.
+            y (int): Coordonnée y du voisin potentiel.
+
+        Retour:
+            bool: True si les coordonnées sont des voisins, sinon False.
+        """
         if (l, h) != (x, y):
             return abs(l - x) <= 1 and abs(h - y) <= 1
 
-    def get_voisins(self, l, h):
-        voisins = []  #liste de Cellules
+    def get_voisins(self, l:int, h:int)->list:
+        """
+        Récupère la liste des cellules voisines de la cellule aux coordonnées (l, h).
+
+        Récupère la liste des cellules voisines de la cellule spécifiée en parcourant la Grille et en vérifiant les voisins.
+
+        Paramètres:
+            l (int): Coordonnée en largeur.
+            h (int): Coordonnée en hauteur.
+
+        Retour:
+            list[Cellule]: Liste des cellules voisines.
+        """
+        voisins = []
         for x in range(self.largeur):
             for y in range(self.hauteur):
                 if self.dans_grille(x, y) and self.est_voisin(l, h, x, y):
@@ -43,11 +153,25 @@ class Grille:
         return voisins
 
     def affecte_voisins(self):
+        """
+        Affecte les voisins de chaque cellule dans la grille.
+
+        Affecte la liste des voisins pour chaque cellule dans la grille en utilisant la méthode get_voisins.
+
+        """
         for l in range(self.largeur):
             for h in range(self.hauteur):
                 self.matrix[h][l].set_voisins(self.get_voisins(l, h))
 
-    def __str__(self):
+    def __str__(self)->str:
+        """
+        Convertit la grille en une chaîne de caractères pour l'affichage.
+
+        Convertit la grille en une chaîne de caractères où chaque cellule vivante est représentée par "X" et chaque cellule morte est représentée par "-".
+
+        Retour:
+            str: La représentation de la grille sous forme de chaîne de caractères.
+        """
         ret = ""
         for h in self.matrix:
             ret += "\n"
@@ -55,7 +179,15 @@ class Grille:
                 ret += l.__str__()
         return ret
 
-    def remplir_alea(self, taux):
+    def remplir_alea(self, taux:int):
+        """
+        Remplit la grille de manière aléatoire.
+
+        Remplit la grille aléatoirement en fonction du taux spécifié en utilisant les méthodes naitre et mourir.
+
+        Paramètres:
+            taux (int): Le taux de remplissage en pourcentage.
+        """
         for l in range(self.largeur):
             for h in range(self.hauteur):
                 if randint(1, 100) <= taux:
@@ -64,45 +196,121 @@ class Grille:
                     self.matrix[h][l].mourir()
 
     def jeu(self):
+        """
+        Exécute un cycle du Jeu de la Vie pour chaque cellule dans la grille.
+
+        Exécute un cycle du Jeu de la Vie pour chaque cellule en calculant leur prochain état grâce à calcul_etat_futur.
+        """
         for l in range(self.largeur):
             for h in range(self.hauteur):
                 self.matrix[h][l].calcul_etat_futur()
 
     def actualise(self):
+        """
+        Met à jour l'état actuel des cellules dans la grille.
+
+        Met à jour l'état actuel de chaque cellule dans la grille en utilisant la méthode basculer.
+        """
         for l in range(self.largeur):
             for h in range(self.hauteur):
                 self.matrix[h][l].basculer()
 
 
 class Cellule:
+    """
+    Représente une cellule dans le Jeu de la Vie.
+
+    Attributs:
+        actuel (bool): L'état actuel de la cellule (vivante ou morte).
+        futur (bool): L'état futur de la cellule calculé pour le prochain cycle.
+        voisins (list[Cellule]): Liste des cellules voisines de la cellule.
+
+    Méthodes:
+        __init__(self)
+        est_vivant(self) -> bool
+        set_voisins(self, voisins: list[Cellule])
+        get_voisins(self) -> list[Cellule]
+        naitre(self)
+        mourir(self)
+        basculer(self)
+        __str__(self) -> str
+        calcul_etat_futur(self)
+    """
     def __init__(self):
+        """
+        Initialise une cellule.
+
+        Crée une cellule avec un état initial mort et sans voisins.
+        """
         self.actuel = False
         self.futur = False
         self.voisins = []
 
-    def est_vivant(self):
+    def est_vivant(self)->bool:
+        """
+        Vérifie si la cellule est vivante.
+
+        Vérifie si la cellule est dans un état vivant grâce à l'attribut actuel de la Cellule.
+
+        Retour:
+            bool: True si la cellule est vivante, sinon False.
+        """
         return self.actuel
 
-    def set_voisins(self, voisins):
+    def set_voisins(self, voisins:list):
+        """
+        Affecte la liste des cellules voisines à la cellule.
+
+        Affecte la liste des cellules voisines à la cellule grâce à l'attribut voisins et l'argument du même nom.
+
+        Paramètres:
+            voisins (list[Cellule]): Liste des cellules voisines.
+        """
         self.voisins = voisins
 
-    def get_voisins(self):
+    def get_voisins(self)->list:
+        """
+        Récupère la liste des cellules voisines de la cellule.
+
+        Récupère la liste des cellules voisines de la cellule grâce à l'attribut voisins de la Cellule..
+
+        Retour:
+            list[Cellule]: Liste des cellules voisines.
+        """
         return self.voisins
 
     def naitre(self):
+        """
+        Change l'état futur de la cellule pour indiquer une naissance.
+
+        Change l'état futur de la cellule pour indiquer une naissance grâce à l'attribut futur.
+        """
         self.futur = True
 
     def mourir(self):
+        """
+        Change l'état futur de la cellule pour indiquer une mort.
+
+        Change l'état futur de la cellule pour indiquer une mort grâce à l'attribut mourir.
+        """
         self.futur = False
 
     def basculer(self):
+        """
+        Passe à l'état futur de la cellule.
+
+        Mets la valeur de l'attribut futur à l'attribut actuel.
+        """
         self.actuel = self.futur
 
-    def __str__(self):
+    def __str__(self)->str:
         """
-        Permet d'afficher la cellule dans la console ou dans un appel print
+        Convertit la cellule en une chaîne de caractères pour l'affichage.
 
-        :return: "X" (str) ou "-" (str)
+        Convertit la cellule en une chaîne de caractères où une cellule vivante est représentée par "X"et une cellule morte est représentée par "-".
+
+        Retour:
+            str: La représentation de la cellule sous forme de chaîne de caractères.
         """
         if self.est_vivant():
             return "X"
@@ -110,6 +318,11 @@ class Cellule:
             return "-"   # Tiret du 6
 
     def calcul_etat_futur(self):
+        """
+        Calcule l'état futur de la cellule en fonction de ses voisins.
+
+        Calcule l'état futur de la cellule en fonction du nombre de voisins vivants.
+        """
         if self.est_vivant():
             cellules_vivantes = 0
             for v in self.voisins:
--- main.py
+++ main.py
@@ -1,18 +1,98 @@
 from cellules import *
+import os
 from time import sleep
-
-
 def effacer_ecran():
-    print("\u001B[H\u001B[J")
+    """
+    Efface l'écran de la console.
 
+    Cette fonction utilise la commande système 'cls' sur Windows et 'clear' sur les systèmes Linux.
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-jeuDeLaVie = Grille(30,5)
-jeuDeLaVie.remplir_alea(45)
-jeuDeLaVie.actualise()
-while True:
-    jeuDeLaVie.affecte_voisins()
-    jeuDeLaVie.jeu()
+def infini():
+    """
+    Exécute le Jeu de la Vie de manière infinie.
+
+    Crée une grille de jeu, la remplit aléatoirement en fonction du taux de remplissage,
+    puis met à jour et affiche la grille de manière continue dans une boucle infinie.
+    """
+    jeuDeLaVie = Grille(largeur,hauteur)
+    jeuDeLaVie.remplir_alea(remplissage)
     jeuDeLaVie.actualise()
-    print(jeuDeLaVie)
-    sleep(0.5)
-    effacer_ecran()
+    while True:
+        jeuDeLaVie.affecte_voisins()
+        jeuDeLaVie.jeu()
+        jeuDeLaVie.actualise()
+        effacer_ecran()
+        print(jeuDeLaVie)
+
+def limite(lim):
+    """
+    Exécute le Jeu de la Vie pour un nombre limité de cycles.
+
+    Crée une grille de jeu, la remplit aléatoirement en fonction du taux de remplissage,
+    puis met à jour et affiche la grille pour un nombre spécifié de cycles.
+
+    Paramètres:
+        lim (int): Le nombre de cycles à exécuter.
+    """
+    jeuDeLaVie = Grille(largeur, hauteur)
+    jeuDeLaVie.remplir_alea(remplissage)
+    jeuDeLaVie.actualise()
+    for i in range(lim):
+        jeuDeLaVie.affecte_voisins()
+        jeuDeLaVie.jeu()
+        jeuDeLaVie.actualise()
+        effacer_ecran()
+        print(jeuDeLaVie)
+        if i == lim-1:
+            print("\nFin du Jeu la Vie, extinction du programme dans 10 secondes !\nMerci d'avoir utlisé notre programme !")
+            sleep(10)
+
+
+
+print("\n\nBienvenue sur le Jeu de la Vie en Python")
+
+try:
+    largeur = int(input("\nVeuillez choisir une largeur de cadre (par défaut 50) : "))
+    print(f"Valeur entrée : {largeur}")
+except ValueError:
+    largeur = 50
+    print(f"Valeur invalide, donc valeur de largeur par défaut : {largeur}")
+
+try:
+    hauteur = int(input("\nVeuillez choisir une hauteur de cadre (par défaut 20) : "))
+    print(f"Valeur entrée : {hauteur}")
+except ValueError:
+    hauteur = 20
+    print(f"Valeur invalide, donc valeur de hauteur par défaut : {hauteur}")
+
+try:
+    remplissage = int(input("\nVeuillez choisir un taux de remplissage en pourcentage (par défaut 25) : "))
+    if 0 <= remplissage <= 100:
+        print(f"Taux entré : {remplissage}")
+    else:
+        remplissage = 25
+        print(f"Taux invalide, cela doit être compris entre 0 et 100, donc taux par défaut : {remplissage}")
+except ValueError:
+    remplissage = 25
+    print(f"Taux invalide, donc taux par défaut : {remplissage}")
+
+try:
+    tours = int(input("\nVeuillez choisir le nombre de cycles (par défaut 200) (0 pour infini) : "))
+    print(f"Valeur entrée : {tours}")
+    if tours == 0:
+        print("Lancement !")
+        sleep(1)
+        infini()
+    else:
+        print("Lancement !")
+        sleep(1)
+        limite(tours)
+except ValueError:
+    tours = 200
+    print(f"Valeur invalide, donc nombre de tours par défaut : {tours}")
+    print("Lancement !")
+    sleep(1)
+    limite(tours)
+
